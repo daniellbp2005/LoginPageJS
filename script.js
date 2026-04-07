1e262d4b037b3096e05bed1957ad0b959ee8db9b
DANIEL LOZANO BORGES P�RES <25254373@educ123.sp.senai.br>
2026-04-07 10:44:38 -0300

    ✔ choore: add o back-end 'Funcional' da página de cadastro

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const emailInput = document.getElementById("email");
 const senhaInput = document.getElementById("senha");
-const btn = document.getElementById("btn");
+const nomeInput = document.getElementById("nome");
+const btnLogin = document.getElementById("btnLogin");
+const btnCadastro = document.getElementById("btnCadastro");
 const texto = document.querySelector(".inserir")
 
 let dados = [
@@ -18,20 +20,53 @@ let dados = [
     }
 ];
 
-btn.addEventListener("click", () => {
-    event.preventDefault();
+if (btnLogin) {
+    btnLogin.addEventListener("click", (event) => {
+        event.preventDefault();
 
-    let email = emailInput.value;
-    let senha = senhaInput.value;
+        let email = emailInput.value;
+        let senha = senhaInput.value;
 
-    if (dados.find(aux => aux.senha === senha && aux.email === email)) {
+        if (dados.find(aux => aux.senha === senha && aux.email === email)) {
+            texto.style.color = "green";
+            texto.innerText = "Acesso Liberado";
+        } else if (email === "" || senha === "") {
+            texto.style.color = "red";
+            texto.innerText = "Preencha o Formulárioo";
+        } else {
+            texto.style.color = "red";
+            texto.innerText = "Acesso Negado";
+        }
+    })
+}
+if (btnCadastro) {
+    btnCadastro.addEventListener("click", (event) => {
+        event.preventDefault();
+
+        let email = emailInput.value;
+        let senha = senhaInput.value;
+        let nome = nomeInput.value;
+
+        if (email === "" && nome === "" && senha === "") {
+            texto.innerText = "Preencha o Formulário";
+            texto.style.color = "red"
+            return
+        }
+        let novoUser = {
+            id: dados.length + 1,
+            nome: nome,
+            email: email,
+            senha: senha,
+        }
+        console.log(novoUser);
+        if (dados.find(aux => aux.email === email)) {
+            texto.style.color = "red";
+            texto.innerText = "Esse conta já existe";
+            return
+        }
+        dados.push(novoUser);
+
         texto.style.color = "green";
-        texto.innerText = "Acesso Liberado";
-    }else if(email === "" || senhha === ""){
-        texto.style.color = "red";
-        texto.innerText = "Preencha o Formulárioo";
-    } else {
-        texto.style.color = "red";
-        texto.innerText = "Acesso Negado";
-    }
-})
+        texto.innerText = "Cadastro Realizado com Sucesso..."
+    })
+}
